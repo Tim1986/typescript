d9fe1f387986bcf31e32dec87eaffde48f26a7ff
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-12-16 16:53:26 -0500

    set up types, following traversy typescript crash course

---
+++ types.js
@@ -0,0 +1,8 @@
+// tsc filename
+var myString;
+var myNum;
+var myBool;
+myString = 'Hello World';
+myNum = 22;
+myBool = true;
+console.log(myBool);
---
+++ types.ts
@@ -0,0 +1,29 @@
+// tsc filename
+
+var myString: string;
+var myNum: number;
+var myBool: boolean;
+var myVar: any;
+
+// var strArr: string[]
+var strArr: Array<string>
+var numArr: number[]
+var boolArr: boolean[]
+
+var strNumTuple: [string, number]
+
+myString = 'Hello World';
+myNum = 22;
+myBool = true;
+myVar = 5;
+
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, false, true];
+strNumTuple = ['Hello', 4];
+
+var myVoid: void = null
+var myNull: null = null
+var myUndefined: undefined = undefined
+
+console.log(myVoid)
